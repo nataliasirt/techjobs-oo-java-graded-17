fce3cf45c63928351f23a0a38440e268f90e85d1
nsirtak <nsirtak@gmail.com>
2024-11-18 16:24:27 -0600

    TestTask4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -6,16 +6,21 @@ import static org.junit.Assert.*;
 
 public class JobTest {
     //TODO: Create your unit tests here
+    //TASK 4
+    //test verifies that each Job object created with empty constructor get unique Id.
     @Test
     public void testSettingJobId(){
         Job job1 = new Job();
         Job job2 = new Job();
         assertNotEquals(job1.getId(), job2.getId());
     }
+    //check if full constructor correctly sets all fiels and types
 @Test
     public void testJobConstructorSetsAllFields() {
     Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    //check if testJob1 is instance of Job
     assertTrue(testJob1 instanceof Job);
+    //check name field etc
     assertEquals("Product tester", testJob1.getName());
 
     assertTrue(testJob1.getEmployer() instanceof Employer);
@@ -30,13 +35,14 @@ public class JobTest {
     assertTrue(testJob1.getCoreCompetency() instanceof CoreCompetency);
     assertEquals("Persistence", testJob1.getCoreCompetency().getValue());
 }
-
+//checks jobs with the same field values but different ids are not equal
 @Test
         public void testJobsForEquality(){
         Job job1 = new Job ("Web Developer", new Employer("Google"), new Location("St. Louis"), new PositionType("Software Engineer"), new CoreCompetency("Java"));
         Job job2 = new Job ("Web Developer", new Employer("Google"), new Location("St. Louis"), new PositionType("Software Engineer"), new CoreCompetency("Java"));
+
         assertFalse(job1.equals(job2));
     }
-
 }
+
 
