a676f6140faf3c8114833b7ac597c754823ee08a
nsirtak <nsirtak@gmail.com>
2024-11-18 13:37:50 -0600

    TestTask3

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,4 +1,8 @@
 package org.launchcode.techjobs.oo;
+import org.junit.Test;
+
+import static java.lang.System.lineSeparator;
+import static org.junit.Assert.*;
 
 public class JobTest {
     //TODO: Create your unit tests here
